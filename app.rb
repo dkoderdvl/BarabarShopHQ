20251aad40b2868e82329b72509eb4aee3c34be5
dkoderdvl <dkoder.dvl@gmail.com>
2019-02-20 23:50:55 +0300

    gems adn first Rakefile

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sqlite3'
+require 'sinatra/activerecord'
+#require 'sqlite3'
+
+set :database, "sqlite3:barbarshop.db"
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
   erb "!!Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a> #{request.path}"
-end
-
-get '/new' do
-  erb :new
 end
