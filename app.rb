158b815966637a70936498e5c4bb5da417fd458f
dkoderdvl <dkoder.dvl@gmail.com>
2019-02-20 23:18:47 +0300

    update gemfile

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-#encoding: utf-8
-
-
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
   erb "!!Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a> #{request.path}"
